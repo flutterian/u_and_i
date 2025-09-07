55bdf9566f110098e81a27e2aec73dc587a9f4b8
BYUNGMIN CHOI <bmchoi803@gmail.com>
2025-09-08 07:06:34 +0900

    _DDay 위젯, _CoupleImage 위젯 위치 설정

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,31 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Home Screen'));
+    return Scaffold(
+      body: SafeArea(
+        top: true,
+        bottom: false,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [_DDay(), _CoupleImage()],
+        ),
+      ),
+    );
+  }
+}
+
+// 이름 첫글자가 언더스코어이면 다른 파일에서 접근 불가
+class _DDay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('DDay Widget');
+  }
+}
+
+class _CoupleImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Couple Image Widget');
   }
 }
