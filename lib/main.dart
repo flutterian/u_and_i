b645dd4189172a4a5caed34a0d720aa9fd7d4eee
BYUNGMIN CHOI <bmchoi803@gmail.com>
2025-09-10 07:09:59 +0900

    스타일(폰트 및 색상) 적용 및 오버플로 해결

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:u_and_i/screen/home_screen.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomeScreen()));
+  // build() 함수에 입력되지 않은 값들은 핫 리로드 반영이 되지 않는다.
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 80.0,
+            fontWeight: FontWeight.w700,
+            fontFamily: 'parisienne',
+          ),
+          displayMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyLarge: TextStyle(color: Colors.white, fontSize: 30.0),
+          bodyMedium: TextStyle(color: Colors.white, fontSize: 20.0),
+        ),
+      ),
+      home: HomeScreen(),
+    ),
+  );
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -24,21 +24,29 @@ class HomeScreen extends StatelessWidget {
 class _DDay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // 테마 불러오기
+    final textTheme = Theme.of(
+      context,
+    ).textTheme; // 위젯 트리 위 가장 가까운 Theme 값을 가져온다.
+
     return Column(
       children: [
         const SizedBox(height: 16.0),
-        Text('U&I'),
+        Text('U&I', style: textTheme.displayLarge),
         const SizedBox(height: 16.0),
-        Text('우리 처음 만난 날'),
-        Text('2013.03.22'), // 임시로 하드 코딩
+        Text('우리 처음 만난 날', style: textTheme.bodyLarge),
+        Text('2013.03.22', style: textTheme.bodyMedium), // 임시로 하드 코딩
         const SizedBox(height: 16.0),
         IconButton(
           iconSize: 60.0,
           onPressed: () {},
-          icon: Icon(Icons.favorite),
+          icon: Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ), // Icon의 경우 색상이 각각 다른 경우가 많아서 직접 지정
         ),
         const SizedBox(height: 16.0),
-        Text('D+4555'),
+        Text('D+4555', style: textTheme.displayMedium),
       ],
     );
   }
@@ -47,10 +55,13 @@ class _DDay extends StatelessWidget {
 class _CoupleImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(
-        'asset/img/middle_image.png',
-        height: MediaQuery.of(context).size.height / 2,
+    return Expanded(
+      // Expaneded 로 오버플로우 해결, 이미지가 남는 공간만큼만 차지하도록해준다. 위의 글자가 절반 이상을 차지하면서 생기는 문제.
+      child: Center(
+        child: Image.asset(
+          'asset/img/middle_image.png',
+          height: MediaQuery.of(context).size.height / 2,
+        ),
       ),
     );
   }
